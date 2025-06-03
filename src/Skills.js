ec0d8333f9a2eaa84b4ac8e1f272a113cefc315c
Narain Sriram <narainsriram@gmail.com>
2025-06-03 17:37:13 -0400

    edits

--- src/Skills.js
+++ src/Skills.js
@@ -8,140 +8,140 @@ function Skills() {
             name: 'Python',
             logo: '/python.png',
             description: 'High-level, general-purpose programming language',
-            proficiency: 'Advanced',
+            //proficiency: 'Advanced',
             used: 'Employed Python in personal projects for data analysis with NumPy and Pandas'
         },
         {
             name: 'Java',
             logo: '/java.png',
             description: 'Object-oriented, class-based programming language',
-            proficiency: 'Advanced',
+            //proficiency: 'Advanced',
             used: 'Gained proficiency through academic studies and projects, including developing a GUI-based application'
         },
         {
             name: 'C',
             logo: '/c.png',
             description: 'Low-level, powerful programming language for system and application development',
-            proficiency: 'Advanced',
+            //proficiency: 'Advanced',
             used: 'Utilized C to complete multiple school projects'
         },
         {
             name: 'x86-64 Assembly',
             logo: '/x86(2).png',
             description: 'Low-level language for CPU instruction and memory control',
-            proficiency: 'Advanced',
+            //proficiency: 'Advanced',
             used: 'Utilized x86-64 assembly to complete multiple school projects'
         },
         {
             name: 'JavaScript',
             logo: '/js.png',
             description: 'High-level, dynamic programming language for web development',
-            proficiency: 'Advanced',
+            //proficiency: 'Advanced',
             used: 'Extensively used for web development, including building websites and interactive applications'
         },
         {
             name: 'React JS & React Native',
             logo: '/rn.png',
             description: 'Framework for building cross-platform mobile applications',
-            proficiency: 'Advanced',
+            //proficiency: 'Advanced',
             used: 'Developed multiple mobile apps and a website, showcasing skills in creating responsive user interfaces'
         },
         {
             name: 'Flutter',
             logo: '/fl.png',
             description: 'Google\'s UI toolkit for building natively compiled applications',
-            proficiency: 'Intermediate',
+            //proficiency: 'Intermediate',
             used: 'Developed the frontend of the wAIste app utilizing the Flutter framework'
         },
         {
             name: 'Next.js',
             logo: '/next-js.png',
             description: 'React framework for building fast, server-rendered web applications',
-            proficiency: 'Intermediate',
+            //proficiency: 'Intermediate',
             used: 'Utilized Next.js during my internship at Chapter One'
         },
         {
             name: 'Swift',
             logo: '/swift.png',
             description: 'Language for developing iOS, macOS applications',
-            proficiency: 'Familiar',
+            //proficiency: 'Familiar',
             used: 'Showcased expertise in Swift by building a football table game, combining programming skills with game development concepts'
         },
         {
             name: 'SQL',
             logo: '/sql.png',
             description: 'Programming language for managing and querying relational databases',
-            proficiency: 'Intermediate',
+            //proficiency: 'Intermediate',
             used: 'Utilized MySQL during my Rebel Internship, writing complex queries and designing database schemas'
         },
         {
             name: 'NumPy',
             logo: '/numpy.webp',
             description: 'Python library for scientific computing',
-            proficiency: 'Intermediate',
+            //proficiency: 'Intermediate',
             used: 'Employed for numerical computing in a house price data analysis project'
         },
         {
             name: 'Pandas',
             logo: '/pandas.png',
             description: 'Python library for data manipulation and analysis',
-            proficiency: 'Intermediate',
+            //proficiency: 'Intermediate',
             used: 'Used alongside NumPy for data manipulation and analysis in a house price project'
         },
         {
             name: 'Git',
             logo: '/git.png',
             description: 'Distributed version control system for tracking changes in source code',
-            proficiency: 'Advanced',
+            //proficiency: 'Advanced',
             used: 'Proficient in using Git for version control and collaboration across projects'
         },
         {
             name: 'Unix/Linux',
             logo: '/linux.png',
             description: 'Family of open-source, multi-tasking operating systems',
-            proficiency: 'Advanced',
+            //proficiency: 'Advanced',
             used: 'Proficient in Unix/Linux operating systems, including shell scripting and command-line tools'
         },
         {
             name: 'MATLAB',
             logo: '/matlab.png',
             description: 'Numerical computing environment and programming language',
-            proficiency: 'Intermediate',
+            //proficiency: 'Intermediate',
             used: 'Mastered MATLAB in Linear Algebra, certified in MATLAB ONRAMP, used for various numerical computing tasks'
         },
         {
             name: 'HTML/CSS',
             logo: '/html.png',
             description: 'Language for creating and designing web pages',
-            proficiency: 'Intermediate',
+            //proficiency: 'Intermediate',
             used: 'Proficient in HTML for building web pages and web applications'
         },
         {
             name: 'Google Cloud Platform',
             logo: '/gc.png',
             description: 'Google\'s comprehensive cloud computing platform for various services',
-            proficiency: 'Familiar',
+            //proficiency: 'Familiar',
             used: 'Utilized Google Cloud Platform for backend development in my rebel internship, including App Engine and Cloud SQL'
         },
         {
             name: 'Flask',
             logo: '/flask.png',
             description: 'Python web framework for building web applications and APIs',
-            proficiency: 'Intermediate',
+            //proficiency: 'Intermediate',
             used: 'Used Flask for backend development in the wAIste project, implementing RESTful APIs'
         },
         {
             name: 'Figma',
             logo: '/figma.png',
             description: 'Web-based collaborative tool for UI/UX design and prototyping',
-            proficiency: 'Intermediate',
+            //proficiency: 'Intermediate',
             used: 'Used Figma to create UI/UX designs during my rebel summer internship'
         },
         {
             name: 'Microsoft Office',
             logo: '/office.png',
             description: 'Productivity software for documents, spreadsheets, and presentations',
-            proficiency: 'Advanced',
+            //proficiency: 'Advanced',
             used: 'Proficient in Word, Excel, and PowerPoint for document creation, data analysis, and presentations'
         },
     ];
